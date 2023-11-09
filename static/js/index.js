97d3ef204146b72af7c6a5073c521c82402c14b5
eziaowonder <eziaowonder@gmail.com>
2023-11-09 21:20:02 +0800

    add typing effect example

--- static/js/index.js
+++ static/js/index.js
@@ -76,3 +76,15 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+// Typing Effect borders (fancy)
+document.querySelector('.button').onmousemove = (e) => {
+
+	const x = e.pageX - e.target.offsetLeft
+	const y = e.pageY - e.target.offsetTop
+
+	e.target.style.setProperty('--x', `${ x }px`)
+	e.target.style.setProperty('--y', `${ y }px`)
+	
+}
+
